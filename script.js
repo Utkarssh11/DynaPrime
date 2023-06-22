5474b6293f11b4bceed1d31e2dc74541cbd955e3
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-06-22 10:58:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function nav() {
+	document.getElementById("menu-icon").classList.toggle("menu-icon-open");
+	document.getElementById("close-icon").classList.toggle("close-icon-open");
+
+	document.getElementById("nav").classList.toggle("open");
+	
+}
